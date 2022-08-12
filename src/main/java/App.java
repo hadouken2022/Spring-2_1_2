f26552596c7951ed01c9f78c88a1b7c59b82c907
hadouken2022 <vonmark1996@gmail.com>
2022-08-12 23:10:22 +0300

    pre project Spring 2_1_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloWorld2");
+        Cat catBean =
+                (Cat) applicationContext.getBean("meow");
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("meow2");
+        boolean comparation = bean == bean2;
+        boolean comparationCats = catBean == catBean2;
+        System.out.println(comparation);
+        System.out.println(comparationCats);
     }
 }
